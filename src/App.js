9c6291d4c0c0ef82352d1639597d90a3baae0f83
rahul975337 <tilwani03@gmail.com>
2020-11-03 21:36:46 +0530

    completed 1-useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import UseStateCounter from "./tutorial/1-useState/setup/5-useState-counter";
 
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <UseStateCounter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
